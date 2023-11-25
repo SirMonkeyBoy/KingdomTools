7c4bbb18dad9da781c9f8f92289e9e130653c2a2
SirMonkeyboy <116051515+SirMonkeyBoy@users.noreply.github.com>
2023-11-25 12:02:11 +1100

    Adds Phoenix Pick mostly
    Still need to add lore and maybe other stuff

--- src/main/java/me/sirmonkeyboy/kingdomtools/Items/ItemManager.java
+++ src/main/java/me/sirmonkeyboy/kingdomtools/Items/ItemManager.java
@@ -1,9 +1,15 @@
 package me.sirmonkeyboy.kingdomtools.Items;
 
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.Collections;
 
 import static net.kyori.adventure.text.Component.*;
 
@@ -18,8 +24,14 @@ public class ItemManager {
     private static void createPhoenixPick() {
         ItemStack item = new ItemStack(Material.DIAMOND_PICKAXE);
         ItemMeta mata = item.getItemMeta();
-        mata.displayName(text("Phoenix Pickaxe"));
+        mata.displayName(text("Phoenix Pick").color(TextColor.color(0x0000AA)).decoration(TextDecoration.BOLD, true));
+        mata.lore(Collections.singletonList(text("test").color(TextColor.color(0xAAAAAA))));
         item.setItemMeta(mata);
+        item.addUnsafeEnchantment(Enchantment.DIG_SPEED, 11);
+        item.addUnsafeEnchantment(Enchantment.SILK_TOUCH, 1);
+        item.addUnsafeEnchantment(Enchantment.DURABILITY, 5);
+        item.addUnsafeEnchantment(Enchantment.LOOT_BONUS_BLOCKS, 5);
+        item.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ENCHANTS);
         phoenixpick = item;
     }
 }
