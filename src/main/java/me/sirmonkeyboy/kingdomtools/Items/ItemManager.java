b0ff15a7da93848951a8524830191eab0729d386
SirMonkeyboy <116051515+SirMonkeyBoy@users.noreply.github.com>
2023-12-04 18:36:43 +1100

    Adds Phoenix Axe Combat

--- src/main/java/me/sirmonkeyboy/kingdomtools/Items/ItemManager.java
+++ src/main/java/me/sirmonkeyboy/kingdomtools/Items/ItemManager.java
@@ -28,11 +28,14 @@ public class ItemManager {
 
     public static ItemStack PhoenixRod;
 
+    public static ItemStack PhoenixAxeCombat;
+
     static {
         createPhoenixPickSilk();
         createPhoenixPickFortune();
         createPhoenixShovelSilk();
         createPhoenixAxeSilk();
+        createPhoenixAxeCombat();
         createPhoenixSword();
         createPhoenixRod();
     }
@@ -103,6 +106,31 @@ public class ItemManager {
         item.addUnsafeEnchantment(Enchantment.SILK_TOUCH,1);
         item.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ENCHANTS);
         PhoenixAxeSilk = item;
+    }
+
+    private static void createPhoenixAxeCombat(){
+        ItemStack item = new ItemStack(Material.DIAMOND_AXE);
+        ItemMeta mata = item.getItemMeta();
+        mata.displayName(text("Phoenix Axe").color(TextColor.color(0x0000AA)).decoration(TextDecoration.BOLD, true));
+        ArrayList<Component> lore = new ArrayList<>();
+        lore.add(text("Sharpness VII").color(TextColor.color(0xAAAAAA)));
+        lore.add(text("Smite V").color(TextColor.color(0xAAAAAA)));
+        lore.add(text("Bane of Arthropods V").color(TextColor.color(0xAAAAAA)));
+        lore.add(text("Looting VI").color(TextColor.color(0xAAAAAA)));
+        lore.add(text("Sweeping Edge III").color(TextColor.color(0xAAAAAA)));
+        lore.add(text("Knockback II").color(TextColor.color(0xAAAAAA)));
+        lore.add(text("Unbreaking V").color(TextColor.color(0xAAAAAA)));
+        mata.lore(lore);
+        item.setItemMeta(mata);
+        item.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, 7);
+        item.addUnsafeEnchantment(Enchantment.DAMAGE_UNDEAD, 5);
+        item.addUnsafeEnchantment(Enchantment.DAMAGE_ARTHROPODS,5);
+        item.addUnsafeEnchantment(Enchantment.LOOT_BONUS_MOBS, 6);
+        item.addUnsafeEnchantment(Enchantment.SWEEPING_EDGE, 3);
+        item.addUnsafeEnchantment(Enchantment.KNOCKBACK, 2);
+        item.addUnsafeEnchantment(Enchantment.DURABILITY, 5);
+        item.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ENCHANTS);
+        PhoenixAxeCombat = item;
     }
 
     private static void createPhoenixSword() {
