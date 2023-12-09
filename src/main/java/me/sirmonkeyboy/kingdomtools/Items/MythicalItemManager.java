7e158e25d4e2812260a728bc1f8b5fc2fb64d82b
SirMonkeyboy <116051515+SirMonkeyBoy@users.noreply.github.com>
2023-12-09 18:11:24 +1100

    Adds Mythical item manager

---
+++ src/main/java/me/sirmonkeyboy/kingdomtools/Items/MythicalItemManager.java
@@ -0,0 +1,12 @@
+package me.sirmonkeyboy.kingdomtools.Items;
+
+import org.bukkit.inventory.ItemStack;
+
+public class MythicalItemManager {
+
+    public static ItemStack Mythical;
+
+    static {
+
+    }
+}
