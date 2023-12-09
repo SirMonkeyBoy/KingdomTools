7bb761971ee77ce74fc429c41fd19a9a2ce87094
SirMonkeyboy <116051515+SirMonkeyBoy@users.noreply.github.com>
2023-12-09 18:20:08 +1100

    Adds Mythical Pick Silk

--- src/main/java/me/sirmonkeyboy/kingdomtools/Items/MythicalItemManager.java
+++ src/main/java/me/sirmonkeyboy/kingdomtools/Items/MythicalItemManager.java
@@ -1,12 +1,44 @@
 package me.sirmonkeyboy.kingdomtools.Items;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+
+import static net.kyori.adventure.text.Component.text;
 
 public class MythicalItemManager {
 
-    public static ItemStack Mythical;
+    //Tools
+    public static ItemStack MythicalPickSilk;
 
     static {
+        createMythicalPickSilk();
+    }
 
+    private static void createMythicalPickSilk() {
+        ItemStack item = new ItemStack(Material.DIAMOND_PICKAXE);
+        ItemMeta mata = item.getItemMeta();
+        mata.displayName(text("Mythical Pick").color(TextColor.color(0xAA0000)).decoration(TextDecoration.BOLD, true));
+        ArrayList<Component> lore = new ArrayList<>();
+        lore.add(text("Efficiency VII").color(TextColor.color(0xAAAAAA)));
+        lore.add(text("Silk Touch").color(TextColor.color(0xAAAAAA)));
+        lore.add(text("Mending").color(TextColor.color(0xAAAAAA)));
+        lore.add(text("Unbreaking V").color(TextColor.color(0xAAAAAA)));
+        mata.lore(lore);
+        item.setItemMeta(mata);
+        item.addUnsafeEnchantment(Enchantment.DIG_SPEED, 7);
+        item.addUnsafeEnchantment(Enchantment.SILK_TOUCH, 1);
+        item.addUnsafeEnchantment(Enchantment.MENDING, 1);
+        item.addUnsafeEnchantment(Enchantment.DURABILITY, 5);
+        item.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ENCHANTS);
+        MythicalPickSilk = item;
     }
 }
