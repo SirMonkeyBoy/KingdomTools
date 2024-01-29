274c6b41d174d148d60728b8bc38bcd587f45375
SirMonkeyboy <116051515+SirMonkeyBoy@users.noreply.github.com>
2024-01-29 16:11:14 +1100

    Adds arg to givetoolm

--- src/main/java/me/sirmonkeyboy/kingdomtools/commands/GiveToolMSubCommands/MythicalAxeSilk.java
+++ src/main/java/me/sirmonkeyboy/kingdomtools/commands/GiveToolMSubCommands/MythicalAxeSilk.java
@@ -3,6 +3,7 @@ package me.sirmonkeyboy.kingdomtools.commands.GiveToolMSubCommands;
 import me.sirmonkeyboy.kingdomtools.Items.MythicalItemManager;
 import me.sirmonkeyboy.kingdomtools.commands.SubCommand;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.List;
@@ -26,7 +27,19 @@ public class MythicalAxeSilk extends SubCommand {
     @Override
     public void perform(Player p, String[] args) {
         if (p.hasPermission("KingdomTools.command.giveTools.MythicalAxeSilk")){
-            p.getInventory().addItem(MythicalItemManager.MythicalAxeSilk);
+            if (args.length == 1) {
+                p.getInventory().addItem(MythicalItemManager.MythicalAxeSilk);
+            }else {
+                if (args.length == 2) {
+                    Player targetPlayer = Bukkit.getPlayer(args[1]);
+
+                    if (targetPlayer == null) {
+                        p.sendMessage("Player not found or not online.");
+                    }else {
+                        targetPlayer.getInventory().addItem(MythicalItemManager.MythicalAxeSilk);
+                    }
+                }
+            }
         }
     }
 
--- src/main/java/me/sirmonkeyboy/kingdomtools/commands/GiveToolMSubCommands/MythicalPickFortune.java
+++ src/main/java/me/sirmonkeyboy/kingdomtools/commands/GiveToolMSubCommands/MythicalPickFortune.java
@@ -3,6 +3,7 @@ package me.sirmonkeyboy.kingdomtools.commands.GiveToolMSubCommands;
 import me.sirmonkeyboy.kingdomtools.Items.MythicalItemManager;
 import me.sirmonkeyboy.kingdomtools.commands.SubCommand;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.List;
@@ -26,7 +27,19 @@ public class MythicalPickFortune extends SubCommand {
     @Override
     public void perform(Player p, String[] args) {
         if (p.hasPermission("KingdomTools.command.giveTools.MythicalPickFortune")){
-            p.getInventory().addItem(MythicalItemManager.MythicalPickFortune);
+            if (args.length == 1) {
+                p.getInventory().addItem(MythicalItemManager.MythicalPickFortune);
+            }else {
+                if (args.length == 2) {
+                    Player targetPlayer = Bukkit.getPlayer(args[1]);
+
+                    if (targetPlayer == null) {
+                        p.sendMessage("Player not found or not online.");
+                    }else {
+                        targetPlayer.getInventory().addItem(MythicalItemManager.MythicalPickFortune);
+                    }
+                }
+            }
         }
     }
 
